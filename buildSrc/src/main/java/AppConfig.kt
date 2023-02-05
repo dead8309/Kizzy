87b796f39ca053ac9f5d55582d4b5575965b6a94
Kizzy <vaibhavmonu8309@gmail.com>
2023-02-05 22:27:33 +0000

    feat(kotlin-dsl): add kotlin-dsl buildSrc folder
    add AppConfig, AppDependencies, Versions

---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,19 @@
+/*
+ *
+ *  ******************************************************************
+ *  *  * Copyright (C) 2022
+ *  *  * AppConfig.kt is part of Kizzy
+ *  *  *  and can not be copied and/or distributed without the express
+ *  *  * permission of yzziK(Vaibhav)
+ *  *  *****************************************************************
+ *
+ *
+ */
+
+object AppConfig {
+    const val compileSdk = 33
+    const val minSdk = 26
+    const val targetSdk = 33
+    const val versionCode = 4
+    const val versionName = "4.1"
+}
