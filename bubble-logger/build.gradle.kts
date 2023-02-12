9d156bf9d41839e97d6ae1b0da99ed8d3a97cd0b
Vaibhav <68665948+dead8309@users.noreply.github.com>
2023-02-12 09:06:14 +0530

    Revert "feat(kotlin-dsl): migrate project from Groovy to Kotlin Dsl"
    This reverts commit 15b7712ba487585319e6373973606e3941b4cc77.

--- bubble-logger/build.gradle.kts
+++ bubble-logger/build.gradle.kts
@@ -49,5 +49,13 @@ android {
 }
 
 dependencies {
-    implementation(AppDependencies.loggerLibraries)
+    implementation("androidx.activity:activity-ktx:1.6.1")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.0")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
