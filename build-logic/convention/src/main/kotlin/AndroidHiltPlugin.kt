866b9ac67921c409fa12143332a876fc6858275b
Kizzy <vaibhavmonu8309@gmail.com>
2023-05-12 13:58:32 +0000

    setup build-logic for convention plugins

---
+++ build-logic/convention/src/main/kotlin/AndroidHiltPlugin.kt
@@ -0,0 +1,26 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidHiltPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("dagger.hilt.android.plugin")
+                // KAPT must go last to avoid build warnings.
+                // See: https://stackoverflow.com/questions/70550883/warning-the-following-options-were-not-recognized-by-any-processor-dagger-f
+                apply("org.jetbrains.kotlin.kapt")
+            }
+
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+            dependencies {
+                "implementation"(libs.findLibrary("hilt").get())
+                "kapt"(libs.findLibrary("hilt.compiler").get())
+            }
+
+        }
+    }
+
+}
---
+++ build-logic/convention/src/main/kotlin/kizzy/gradle/plugin/AndroidCompose.kt
@@ -0,0 +1,32 @@
+package kizzy.gradle.plugin
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+internal fun Project.configureAndroidCompose(
+    commonExtension: CommonExtension<*, *, *, *>,
+) {
+    val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+    commonExtension.apply {
+        buildFeatures {
+            compose = true
+        }
+
+        composeOptions {
+            kotlinCompilerExtensionVersion = libs.findVersion("compose-compiler").get().toString()
+        }
+
+        dependencies {
+            val composeUi = libs.findLibrary("compose-ui").get()
+            val composeUiTooling = libs.findLibrary("compose-ui-tooling").get()
+            val composeUiToolingPreview = libs.findLibrary("compose-ui-tooling-preview").get()
+            add("implementation",composeUi)
+            add("debugImplementation",composeUiTooling)
+            add("debugImplementation",composeUiToolingPreview)
+        }
+    }
+}
