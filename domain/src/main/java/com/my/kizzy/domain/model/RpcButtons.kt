b4b24be26ab5777e0b38cb8c7e0ae39a2e17a12d
Kizzy <vaibhavmonu8309@gmail.com>
2023-05-19 19:25:18 +0000

    fix(domain): fix NPE caused when accessing RpcButtons fields
    gson is setting field values of RpcButtons to null after serialization if the json data passed is ```{}``` which used to work before.
    
    A workaround is implemented using a Custom Deserializer

--- domain/src/main/java/com/my/kizzy/domain/model/RpcButtons.kt
+++ domain/src/main/java/com/my/kizzy/domain/model/RpcButtons.kt
@@ -12,8 +12,14 @@
 
 package com.my.kizzy.domain.model
 
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.annotations.JsonAdapter
 import com.google.gson.annotations.SerializedName
+import java.lang.reflect.Type
 
+@JsonAdapter(RpcButtonsDeserializer::class)
 data class RpcButtons(
     @SerializedName("button1")
     val button1: String = "",
@@ -24,3 +30,23 @@ data class RpcButtons(
     @SerializedName("button2Url")
     val button2Url: String = ""
 )
+
+class RpcButtonsDeserializer: JsonDeserializer<RpcButtons> {
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): RpcButtons {
+        val jsonObject = json?.asJsonObject
+        val button1 = jsonObject?.get("button1")?.asString ?: ""
+        val button1Url = jsonObject?.get("button1Url")?.asString ?: ""
+        val button2 = jsonObject?.get("button2")?.asString ?: ""
+        val button2Url = jsonObject?.get("button2Url")?.asString ?: ""
+        return RpcButtons(
+            button1 = button1,
+            button2 = button2,
+            button1Url = button1Url,
+            button2Url = button2Url
+        )
+    }
+}
