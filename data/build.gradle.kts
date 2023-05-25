f532ebd7c3245775df7fbb208aa395e018ce9436
Kizzy <vaibhavmonu8309@gmail.com>
2023-05-25 15:01:01 +0000

    add unit tests

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,20 +1,30 @@
 plugins {
     id("kizzy.android.library")
     id("kizzy.android.hilt")
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
     namespace = "com.my.kizzy.data"
+    defaultConfig {
+        buildConfigFieldFromGradleProperty("BASE_URL","BASE_URL")
+    }
 }
 
 dependencies {
     implementation (libs.core.ktx)
     implementation (projects.domain)
-    implementation (libs.bundles.network.okhttp)
-    implementation (libs.bundles.network.retrofit)
-    implementation (libs.gson)
+    implementation (libs.bundles.network.ktor.request)
     implementation (libs.javax)
     implementation (projects.common.preference)
     implementation (projects.gateway)
     implementation (libs.blankj.utilcodex)
+    testImplementation(libs.junit)
+}
+
+fun com.android.build.api.dsl.LibraryDefaultConfig.buildConfigFieldFromGradleProperty(fieldName: String, gradlePropertyName: String) {
+    val propertyValue = project.properties[gradlePropertyName] as? String
+    if (propertyValue != null) {
+        buildConfigField("String", fieldName, propertyValue)
+    }
 }
