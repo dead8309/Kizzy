9c4da510eaebdb2ff1342322137c5c0b6e873d45
Kizzy <vaibhavmonu8309@gmail.com>
2023-06-02 10:47:22 +0000

    refactor(feature_rpc_base): use single notification channel for all services

---
+++ feature_rpc_base/src/main/java/com/my/kizzy/feature_rpc_base/Constants.kt
@@ -0,0 +1,21 @@
+/*
+ *
+ *  ******************************************************************
+ *  *  * Copyright (C) 2022
+ *  *  * Constants.kt is part of Kizzy
+ *  *  *  and can not be copied and/or distributed without the express
+ *  *  * permission of yzziK(Vaibhav)
+ *  *  *****************************************************************
+ *
+ *
+ */
+
+package com.my.kizzy.feature_rpc_base
+
+internal object Constants {
+    const val CHANNEL_ID = "kizzy.notification"
+    const val CHANNEL_NAME = "Kizzy Rpc"
+    const val CHANNEL_DESCRIPTION = "Background Service notification which runs rpc"
+    const val NOTIFICATION_ID = 2022_03_04
+    const val ACTION_STOP_SERVICE = "Stop Service"
+}
